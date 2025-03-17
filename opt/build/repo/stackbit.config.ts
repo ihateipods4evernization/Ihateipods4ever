918bab069ff3c3fb32f649cfae87a7b49b912b45
Ihateipods4ever <142383391+Ihateipods4ever@users.noreply.github.com>
2025-03-17 03:48:06 -0500

    Add files via upload
    Signed-off-by: Ihateipods4ever <142383391+Ihateipods4ever@users.noreply.github.com>

--- opt/build/repo/stackbit.config.ts
+++ opt/build/repo/stackbit.config.ts
@@ -1,1 +1,53 @@
-<html><head></head><body style="overflow-wrap: break-word; -webkit-nbsp-mode: space; line-break: after-white-space;"><div>import { ContentfulContentSource } from '@stackbit/cms-contentful'</div><div>import {</div><div>&nbsp; defineStackbitConfig,</div><div>&nbsp; getLocalizedFieldForLocale,</div><div>&nbsp; SiteMapEntry,</div><div>} from '@stackbit/types'</div><div><br></div><div>export default defineStackbitConfig({</div><div>&nbsp; stackbitVersion: '~0.6.0',</div><div>&nbsp; contentSources: [new ContentfulContentSource(/* ... */)],</div><div>&nbsp; modelExtensions: [{ name: 'page', type: 'page', urlPath: '/{slug}' }],</div><div>&nbsp; siteMap: ({ documents, models }) =&gt; {</div><div>&nbsp; &nbsp; const pageModels = models</div><div>&nbsp; &nbsp; &nbsp; .filter((m) =&gt; m.type === 'page')</div><div>&nbsp; &nbsp; &nbsp; .map((m) =&gt; m.name)</div><div>&nbsp; &nbsp; return documents</div><div>&nbsp; &nbsp; &nbsp; .filter((d) =&gt; pageModels.includes(d.modelName))</div><div>&nbsp; &nbsp; &nbsp; .map((document) =&gt; {</div><div>&nbsp; &nbsp; &nbsp; &nbsp; const slug = getLocalizedFieldForLocale(document.fields.slug)</div><div>&nbsp; &nbsp; &nbsp; &nbsp; if (!slug.value) return null</div><div>&nbsp; &nbsp; &nbsp; &nbsp; const urlPath = '/' + slug.value.replace(/^\/+/, '')</div><div>&nbsp; &nbsp; &nbsp; &nbsp; return {</div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; stableId: document.id,</div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; urlPath,</div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; document,</div><div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; isHomePage: urlPath === '/',</div><div>&nbsp; &nbsp; &nbsp; &nbsp; }</div><div>&nbsp; &nbsp; &nbsp; })</div><div>&nbsp; &nbsp; &nbsp; .filter(Boolean) as SiteMapEntry[]</div><div>&nbsp; },</div><div>})</div><div><br></div></body></html>
+<html><head></head><body style="overflow-wrap: break-word; -webkit-nbsp-mode: space; line-break: after-white-space;"><span style="caret-color: rgb(0, 0, 0); color: rgb(0, 0, 0); font-family: system-ui, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, Helvetica, Arial, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;; font-size: medium; white-space: pre; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); -webkit-text-size-adjust: 100%;">// stackbit.config.ts
+import { defineStackbitConfig, SiteMapEntry } from "@stackbit/types";
+
+export default defineStackbitConfig({
+  // ...
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          type: "page",
+          // Static URL path derived from the "slug" field
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        },
+        // ...
+      ],
+    })
+  ],
+  siteMap: ({ documents, models }) =&gt; {
+    // 1. Filter all page models
+    const pageModels = models.filter((m) =&gt; m.type === "page")
+
+    return documents
+      // 2. Filter all documents which are of a page model
+      .filter((d) =&gt; pageModels.some(m =&gt; m.name === d.modelName))
+      // 3. Map each document to a SiteMapEntry
+      .map((document) =&gt; {
+        // Map the model name to its corresponding URL
+        const urlModel = (() =&gt; {
+            switch (document.modelName) {
+                case 'Page':
+                    return 'otherPage';
+                case 'Blog':
+                    return 'otherBlog';
+                default:
+                    return null;
+            }
+        })();
+
+        return {
+          stableId: document.id,
+          urlPath: `/${urlModel}/${document.id}`,
+          document,
+          isHomePage: false,
+        };
+      })
+      .filter(Boolean) as SiteMapEntry[];
+  }
+});</span></body></html>
